e98783d2932bd4ea59c4fa6c123be49c1ee15014
GuilhermeNovaes254 <guilherme.novaes@gmail.com>
2020-07-14 14:56:37 -0300

    Json converted to CSV

---
+++ index.js
@@ -0,0 +1,9 @@
+const jsonexport = require('jsonexport');
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync('example.json'));
+
+jsonexport(data, function(err, csv){
+    if (err) return console.error(err);
+    fs.writeFileSync("data.csv", csv);
+});
